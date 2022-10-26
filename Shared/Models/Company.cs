3e1fb9af957c21c257fedd57488e6d7d386149fe
Jakob Kjellerup <sorenkjellerup@gmail.com>
2022-10-26 22:04:56 +0200

    Early commit of beginning of project

---
+++ Shared/Models/Company.cs
@@ -0,0 +1,17 @@
+﻿namespace Shared;
+
+public class Company
+{
+    public int Id { get; set; }
+    public string Username { get; set; }
+    public string Password { get; set; }
+    public string Email { get; set; }
+
+    public Company(int id, string username, string password, string email)
+    {
+        Id = id;
+        Username = username;
+        Password = password;
+        Email = email;
+    }
+}
---
+++ Shared/Models/Event.cs
@@ -0,0 +1,18 @@
+﻿namespace Shared;
+
+public class Event
+{
+    public int Id { get; set; }
+    public Company Owner { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public DateTime DateTime { get; set; }
+    
+    public Event(Company owner, string title, string description, DateTime dateTime)
+    {
+        Owner = owner;
+        Title = title;
+        Description = description;
+        DateTime = dateTime;
+    }
+}
