9871ae7082a059e133fa12ceb0544129490170b0
Merethe Lundgreen <299922@via.dk>
2022-11-02 14:03:16 +0100

    Added Grpc connection to java

---
+++ GrpcClient/Services/EventService.cs
@@ -0,0 +1,24 @@
+﻿using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using Shared.Models;
+
+namespace GrpcClient.Services;
+
+public class EventService : Greeter.GreeterClient
+{
+    
+    private readonly ILogger<EventService> _logger;
+
+    public EventService(ILogger<EventService> logger)
+    {
+        _logger = logger;
+    }
+
+    public override Task<HelloReply> sayHello(HelloRequest request, ServerCallContext context)
+    {
+        return Task.FromResult(new HelloReply
+        {
+            Message = "Hello " + request.Name
+        });
+    }
+}
